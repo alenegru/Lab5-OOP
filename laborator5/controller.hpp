ff4a8aebd13a411e87da7767ad97ecc29245d5b5
lauragoron <lauragoron@yahoo.com>
2020-04-27 18:43:52 +0300

    created add,update and remove methods for class Controller

--- laborator5/controller.hpp
+++ laborator5/controller.hpp
@@ -10,5 +10,33 @@
 #define controller_hpp
 
 #include <stdio.h>
+#include "repository.hpp"
+#include "watchlist.hpp"
+
+using namespace std;
+
+class Controller
+{
+	private:
+		Repository repo;
+		//WatchList wlist;
+
+	public:
+
+		//for ADMIN
+		void add_movie_to_repo();
+
+		void remove_movie_from_repo();
+
+		void update_movie_from_repo();
+
+		//for USER
+
+
+
+};
+
+
+
 
 #endif /* controller_hpp */
