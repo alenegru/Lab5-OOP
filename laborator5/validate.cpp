95eac8e8e3fdb5628c26731825a04791716e2058
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-04-28 17:52:34 +0300

    Created .cpp file for the 2 methods.

--- laborator5/validate.cpp
+++ laborator5/validate.cpp
@@ -7,3 +7,20 @@
 //
 
 #include <stdio.h>
+#include "repository.hpp"
+#include "validate.hpp"
+#include "watchlist.hpp"
+
+bool validate::validate_unique_repo(int id_, Repository& repo) {
+    //checks if the film already exists
+    if (repo.find_movie_by_id(id_) == true)
+        return false;
+    return true;
+}
+
+bool validate::validate_unique_wlist(int id_, Watchlist& watchlist) {
+    //checks if the film already exists
+    if (watchlist.find_movie_by_id(id_) == true)
+        return false;
+    return true;
+}
