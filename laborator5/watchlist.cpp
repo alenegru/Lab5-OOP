2af96c3db7026c8f1eadf4a0cf39241a40c1651a
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-04-28 17:47:25 +0300

    Added functionalities for the watchlist.

--- laborator5/watchlist.cpp
+++ laborator5/watchlist.cpp
@@ -7,3 +7,41 @@
 //
 
 #include "watchlist.hpp"
+#include "repository.hpp"
+#include "validate.hpp"
+using namespace std;
+
+bool Watchlist::remove_movie_from_watchlist(int id_) {
+    if (isEmpty() == false)
+        for (int i = 0; i < watchlist.size(); i++)
+            if (watchlist[i] == id_) {
+                watchlist.erase(watchlist.begin() + i);
+                return true;
+            }
+    return false;
+}
+
+//adds movie to watchlist
+bool Watchlist::add_movie_to_watchlist(int id_) {
+    validate v;
+
+    if (!(v.validate_unique_wlist(id_, *this)))
+        //movie exists already
+        return false;
+    else
+        watchlist.push_back(id_);
+    return true;
+}
+
+bool Watchlist::isEmpty() {
+    if (watchlist.empty())
+        return true;
+    return false;
+}
+
+bool Watchlist::find_movie_by_id(int id_) {
+    for (int i = 0; i < watchlist.size(); i++)
+        if (watchlist[i] == id_)
+            return true;
+    return false;
+}
