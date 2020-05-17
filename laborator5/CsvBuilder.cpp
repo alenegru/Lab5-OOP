0d644dcb84fdb5dbf171aa36a5c8be7011643306
Alexandra Negru <negru_alexandra90@yahoo.com>
2020-05-17 16:59:25 +0300

    Small changes.

--- laborator5/CsvBuilder.cpp
+++ laborator5/CsvBuilder.cpp
@@ -12,15 +12,15 @@
 using namespace std;
 
 void CsvBuilder::build_watchlist(vector <int> watchlist, Repository &repo) {
-    ofstream fout;
-    fout.open("watchlist.csv");
+    ofstream fin;
+    fin.open("watchlist.csv");
 
-    fout << "#,Title,Genre,Year,Likes,Trailer\n";
+    fin << "#,Title,Genre,Year,Likes,Trailer\n";
 
     for (int i = 0; i < watchlist.size(); i++) {
         Movie movie = repo.get_movie_by_id(watchlist[i]);
-        fout << i + 1 << "," << movie.get_title() << "," << movie.get_genre() << "," << movie.get_year() << "," << movie.get_number_of_likes() << "," << movie.get_trailer() << '\n';
+        fin << i + 1 << "," << movie.get_title() << "," << movie.get_genre() << "," << movie.get_year() << "," << movie.get_number_of_likes() << "," << movie.get_trailer() << "\n";
     }
     
-    fout.close();
+    fin.close();
 }
